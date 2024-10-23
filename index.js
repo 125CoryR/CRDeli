d7516d88069c5cea17877c6eb91ab8bdc777cc46
Cory Reavis <125creavis@frhsd.com>
2024-10-23 12:54:42 -0400

    Made new page and working buttons

---
+++ index.js
@@ -0,0 +1,11 @@
+function ClassicPage(){
+    window.location.href = "Spooky_Classic";
+}
+function ClubPage(){
+    window.location.href = "Spooky_Club";
+}
+function VegPage(){
+    window.location.href = "Spooky_Veg";
+}
+
+
